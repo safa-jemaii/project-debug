c706428f4f83bf9d64ebe4482a56fe8fec2a809a
safa-jemaii <safajemaii48@gmail.com>
2022-12-16 21:32:48 +0100

    first commit

---
+++ src/components/CharacterMap/CharacterMap.js
@@ -0,0 +1,43 @@
+import React, { memo, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { TextContext } from '../App/App';
+
+
+function itemize(text){
+    const letters = text.split('')
+      .filter(l => l !== ' ')
+      .reduce((collection, item) => {
+        const letter = item.toLowerCase();
+        return {
+          ...collection,
+          [letter]: (collection[letter] || 0) + 1
+        }
+      }, {})
+    return Object.entries(letters)
+      .sort((a, b) => b[1] - a[1]);
+   }
+
+
+export default function CharacterMap({ show }) {
+  const text = useContext(TextContext);
+
+  if(!show) {
+    return null;
+  }
+
+  return(
+    <div>
+      Character Map: {text.length}
+      {itemize(text).map(character => (
+       <div key={character[0]}>
+         {character[0]}: {character[1]}
+       </div>
+     ))}
+    </div>
+  )
+}
+
+CharacterMap.proTypes = {
+  show: PropTypes.bool.isRequired
+}
+// export default memo(CharacterMap);
